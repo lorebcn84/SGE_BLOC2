9f2b1fbe415dfec29831a45e43e034eb8b7211c5
Lorena Peiró <2024_lorena.peiro@iticbcn.cat>
2025-02-09 20:48:19 +0100

    Update main.py

--- bloc2_LorenaPeiro/postgresql_python/main.py
+++ bloc2_LorenaPeiro/postgresql_python/main.py
@@ -4,20 +4,21 @@ import update_registre as ur
 import delete_registre as dr
 
 #Trucada per executar la funció a l'arxiu create_registre.py
-#cr.create_reg()
+cr.create_reg()
 
 #afegim la trucada a read_reg
-#results = rr.read_reg()
-#for i in results:
-#    print('\n')
-#    print('Nom: ' + i[0])
-#    print('Adreça: ' + i[1])
-#    print('telèfon: ' + i[2])
-#    print('email: ' + i[3])
-#    print('naixement: ' + i[4])
+results = rr.read_reg()
+print(type(results))
+for i in results:
+    print('\n')
+    print('Nom: ' + i[0])
+    print('Adreça: ' + i[1])
+    print('telèfon: ' + i[2])
+    print('email: ' + i[3])
+    print('naixement: ' + i[4])
 
 #afegim trucada de update_reg()
-#ur.update_reg()
+ur.update_reg()
 
 #afegim trucada de delete_reg()
 dr.delete_reg()
